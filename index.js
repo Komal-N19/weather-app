3503343f309c3398fbef875395d9261b2912fa9f
Komal Nangia <komalnangia05@gmail.com>
2023-08-28 21:22:44 +0530

    added id selectors

--- index.js
+++ index.js
@@ -38,12 +38,12 @@ axios.get(apiUrl).then(displayTemperature);
 }
 function handleSubmit(event){
     event.preventDefault();
-    cityInputElement = document.querySelector("city-input");
+    let cityInputElement = document.querySelector("#city-input");
     search(cityInputElement.value);
-    console.log(cityInputElement.value);
+    
 }
 
 search("New York");
 
-let form = document.querySelector("search-form");
+let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
